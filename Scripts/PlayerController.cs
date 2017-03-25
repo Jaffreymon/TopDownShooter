1cc28a75056c7bf6b47b01a8cfd8cd41392e5283
Jeffrey <jyen@ucsd.edu>
2017-03-24 18:05:53 -0700

    Initial World Layout

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+    // Player Handling
+    [SerializeField]
+    private float rotationSpeed = 475f;
+    [SerializeField]
+    private float walkSpeed = 4f;
+    [SerializeField]
+    private float runSpeed = 7f;
+
+    // System variables
+    private Quaternion targetRotation;
+
+    // Game Components
+    private CharacterController playerController;
+
+    // Use this for initialization
+    void Start () {
+        playerController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+
+        if (input != Vector3.zero)
+        {
+            targetRotation = Quaternion.LookRotation(input);
+            transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
+        }
+
+        Vector3 motion = input;
+        // Corrects player movement speed at diagnols
+        motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1) ? 0.7f : 1f;
+        // Increases player movement based on running or walking
+        motion *= (Input.GetButton("Run")) ? runSpeed : walkSpeed;
+        motion += Vector3.up * -8;
+
+        playerController.Move(motion * Time.deltaTime);
+	}
+}
